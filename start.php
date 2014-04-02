046edfd362551ee3edc3f5ced55fb7d80f1c1f53
michael34435 <michael34435@gamil.com>
2014-04-03 01:15:39 +0800

    mutiple config support i.e. CONFIG=1,2,3,4

--- start.php
+++ start.php
@@ -23,101 +23,105 @@ require_once "ZipTool.php";
 
 define('NET_SSH2_LOGGING', NET_SSH2_LOG_COMPLEX);
 
-$c_file = getenv("CONFIG");
-$config = null;
+$config_file = getenv("CONFIG");
+$config = null;	
 
-if (empty($c_file)) {
-	if (!file_exists("config.json")) {
-		exit("Cannot find config file.");
+foreach (explode(",", $config_file) as $c_key => $c_file) {
+	if (empty($c_file)) {
+		if (!file_exists("config.json")) {
+			exit("Cannot find config file.");
+		} else {
+			$config = json_decode(file_get_contents("config.json"), true);
+		}
 	} else {
-		$config = json_decode(file_get_contents("config.json"), true);
-	}
-} else {
-	if (!file_exists("config$c_file.json")) {
-		exit("Cannot find config file.");
+		if (!file_exists("config$c_file.json")) {
+			exit("Cannot find config file.");
+		} else {
+			$config = json_decode(file_get_contents("config$c_file.json"), true);
+		}
+	}	
+
+	$reset = getenv("RESET");
+	if (empty($reset)) {
+		$reset = "0";
+	}	
+
+	$log = getenv("LOG");
+	$html = getenv("HTML");	
+
+	$hosts = isset($config["hosts"]) ? $config["hosts"] : null;
+	$port = isset($config["port"]) ? $config["port"] : 22;
+	$package = isset($config["package"]) ? $config["package"] : null;
+	$ppk = isset($config["private_key"]) ? $config["private_key"] : null;
+	$user = isset($config["user"]) ? $config["user"] : null;
+	$destination = isset($config["destination"]) ? $config["destination"] : null;
+	$pass = isset($ppk) && !file_exists($ppk) && isset($config["password"]) ? 
+		$config["password"] : null;	
+
+	if (!isset($user) || !isset($hosts) || !isset($package) 
+			|| !is_dir($package) || !is_array($hosts) || count($hosts) == 0
+				|| (isset($ppk) && !file_exists($ppk)) || !isset($destination)) {
+		exit("Config File:config$c_file.json Error ...");
+	}	
+
+	$zipfile = new ZipTool();
+	$zipfile->addNewFile($package);	
+
+	if (isset($ppk) && file_exists($ppk)) {
+		$key = new Crypt_RSA();
+		$key->loadKey(file_get_contents($ppk));
+	} elseif (isset($pass)) {
+		$key = $pass;
 	} else {
-		$config = json_decode(file_get_contents("config$c_file.json"), true);
-	}
-}
+		exit("No key found...");
+	}	
 
-$reset = getenv("RESET");
-if (empty($reset)) {
-	$reset = "0";
-}
+	foreach ($hosts as $number => $host) {	
 
-$log = getenv("LOG");
-$html = getenv("HTML");
+		echo "Start connecting sftp service...$user@$host", PHP_EOL;
+		$sftp = new Net_SFTP($host);
+		if (!$sftp->login($user, $key)) {
+	    	echo "SFTP Login Failed!", PHP_EOL;
+	    	unset($sftp);
+	    	continue;
+		}
+		$sftp->enablePTY(); 
+		echo "Start uploading tmp file to the server... from (" . $zipfile->getFileName() . ")", PHP_EOL;	
 
-$hosts = isset($config["hosts"]) ? $config["hosts"] : null;
-$port = isset($config["port"]) ? $config["port"] : 22;
-$package = isset($config["package"]) ? $config["package"] : null;
-$ppk = isset($config["private_key"]) ? $config["private_key"] : null;
-$user = isset($config["user"]) ? $config["user"] : null;
-$destination = isset($config["destination"]) ? $config["destination"] : null;
-$pass = isset($ppk) && !file_exists($ppk) && isset($config["password"]) ? 
-	$config["password"] : null;
+		$sftp->put($sftp->pwd() . "/tmp.zip", realpath($zipfile->getFileName()), NET_SFTP_LOCAL_FILE );
+		echo "Start installing ... ", PHP_EOL;	
 
-if (!isset($user) || !isset($hosts) || !isset($package) 
-		|| !is_dir($package) || !is_array($hosts) || count($hosts) == 0
-			|| (isset($ppk) && !file_exists($ppk)) || !isset($destination)) {
-	exit("Error ...");
-}
-
-$zipfile = new ZipTool();
-$zipfile->addNewFile($package);
-
-if (isset($ppk) && file_exists($ppk)) {
-	$key = new Crypt_RSA();
-	$key->loadKey(file_get_contents($ppk));
-} elseif (isset($pass)) {
-	$key = $pass;
-} else {
-	exit("No key found...");
-}
-
-foreach ($hosts as $number => $host) {
-
-	echo "Start connecting sftp service...$user@$host", PHP_EOL;
-	$sftp = new Net_SFTP($host);
-	if (!$sftp->login($user, $key)) {
-    	exit("SFTP Login Failed!");
-	}
-	$sftp->enablePTY(); 
-	echo "Start uploading tmp file to the server... from (" . $zipfile->getFileName() . ")", PHP_EOL;
-
-	$sftp->put($sftp->pwd() . "/tmp.zip", realpath($zipfile->getFileName()), NET_SFTP_LOCAL_FILE );
-	echo "Start installing ... ", PHP_EOL;
-
-	$sftp->enableQuietMode();
-	if ($reset == "1") {
-		echo "sudo rm -rf $destination,", $sftp->exec("sudo rm -rf $destination"), ",OK", PHP_EOL;
+		$sftp->enableQuietMode();
+		if ($reset == "1") {
+			echo "sudo rm -rf $destination,", $sftp->exec("sudo rm -rf $destination"), ",OK", PHP_EOL;
+			echo $sftp->read(), PHP_EOL;
+		}
+		echo "sudo mkdir -p $destination,", $sftp->exec("sudo mkdir -p $destination"), ",OK", PHP_EOL;
 		echo $sftp->read(), PHP_EOL;
-	}
-	echo "sudo mkdir -p $destination,", $sftp->exec("sudo mkdir -p $destination"), ",OK", PHP_EOL;
-	echo $sftp->read(), PHP_EOL;
-	echo "sudo mv -f " . $sftp->pwd() . "/tmp.zip $destination,", $sftp->exec("sudo mv -f " . $sftp->pwd() . "/tmp.zip $destination"), ",OK", PHP_EOL;
-	echo $sftp->read(), PHP_EOL;
-	echo "sudo unzip -o $destination/tmp.zip -d $destination,", $sftp->exec("sudo unzip -o $destination/tmp.zip -d $destination"), ",OK", PHP_EOL;
-	echo $sftp->read(), PHP_EOL;
-	echo "sudo rm -rf $destination/tmp.zip,", $sftp->exec("sudo rm -rf $destination/tmp.zip"), ",OK", PHP_EOL;
-	echo $sftp->read(), PHP_EOL;
-	echo "sudo usermod -a -G www-data $user,", $sftp->exec("sudo usermod -a -G www-data $user"), ",OK", PHP_EOL;
-	echo $sftp->read(), PHP_EOL;
-	echo "sudo chgrp -R www-data $destination,", $sftp->exec("sudo chgrp -R www-data $destination"), ",OK", PHP_EOL;
-	echo $sftp->read(), PHP_EOL;
-	echo "sudo chmod -R g+rwxs $destination,", $sftp->exec("sudo chmod -R g+rwxs $destination"), ",OK", PHP_EOL;
-	echo $sftp->read(), PHP_EOL;
-	if (!empty($log)) {
-		echo "sudo chmod -R 777 $destination/$log,", $sftp->exec("sudo chmod -R 777 $destination/$log"), ",OK", PHP_EOL;
+		echo "sudo mv -f " . $sftp->pwd() . "/tmp.zip $destination,", $sftp->exec("sudo mv -f " . $sftp->pwd() . "/tmp.zip $destination"), ",OK", PHP_EOL;
 		echo $sftp->read(), PHP_EOL;
-	}
-	if (!empty($html)) {
-		echo "sudo chmod -R 777 $destination/$html,", $sftp->exec("sudo chmod -R 777 $destination/$html"), ",OK", PHP_EOL;
+		echo "sudo unzip -o $destination/tmp.zip -d $destination,", $sftp->exec("sudo unzip -o $destination/tmp.zip -d $destination"), ",OK", PHP_EOL;
 		echo $sftp->read(), PHP_EOL;
+		echo "sudo rm -rf $destination/tmp.zip,", $sftp->exec("sudo rm -rf $destination/tmp.zip"), ",OK", PHP_EOL;
+		echo $sftp->read(), PHP_EOL;
+		echo "sudo usermod -a -G www-data $user,", $sftp->exec("sudo usermod -a -G www-data $user"), ",OK", PHP_EOL;
+		echo $sftp->read(), PHP_EOL;
+		echo "sudo chgrp -R www-data $destination,", $sftp->exec("sudo chgrp -R www-data $destination"), ",OK", PHP_EOL;
+		echo $sftp->read(), PHP_EOL;
+		echo "sudo chmod -R g+rwxs $destination,", $sftp->exec("sudo chmod -R g+rwxs $destination"), ",OK", PHP_EOL;
+		echo $sftp->read(), PHP_EOL;
+		if (!empty($log)) {
+			echo "sudo chmod -R 777 $destination/$log,", $sftp->exec("sudo chmod -R 777 $destination/$log"), ",OK", PHP_EOL;
+			echo $sftp->read(), PHP_EOL;
+		}
+		if (!empty($html)) {
+			echo "sudo chmod -R 777 $destination/$html,", $sftp->exec("sudo chmod -R 777 $destination/$html"), ",OK", PHP_EOL;
+			echo $sftp->read(), PHP_EOL;
+		}
+		echo "sudo chown -R $user $destination,", $sftp->exec("sudo chown -R $user $destination"), ",OK", PHP_EOL;
+		echo $sftp->read(), PHP_EOL;
+		echo "$user@$host finishing...", PHP_EOL;
+		unset($sftp);
 	}
-	echo "sudo chown -R $user $destination,", $sftp->exec("sudo chown -R $user $destination"), ",OK", PHP_EOL;
-	echo $sftp->read(), PHP_EOL;
-	echo "$user@$host finishing...", PHP_EOL;
-	unset($sftp);
 }
 ?>
